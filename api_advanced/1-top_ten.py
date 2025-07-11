021fc14897f6de36e540b262a0354fa547ac9ee9
angekarigirwa <a.karigirwa@alustudent.com>
2025-07-11 22:53:14 +0000

    recursive function

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-"""
-Module to print the titles of the top ten hot posts for a given subreddit.
-"""
+""" return top ten"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the top ten hot posts for a given subreddit.
-    Prints None if the subreddit is invalid or unavailable.
-    """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'python:top_ten_script:v1.0 (by /u/yourusername)'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            print(None)
-            return
-        json_data = response.json()
-        posts = json_data.get('data', {}).get('children', [])
-        for post in posts[:10]:
-            print(post['data']['title'])
-    except Exception:
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if not response.ok:
         print(None)
+        return
+    json_data = response.json()
+    posts = json_data['data']['children']
+    for i in range(min(10, len(posts))):
+        print(posts[i]['data']['title'])
 
 
