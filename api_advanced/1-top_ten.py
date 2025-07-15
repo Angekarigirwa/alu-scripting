06b3db7e65418e1a450f980da3d91080f2154f90
angekarigirwa <a.karigirwa@alustudent.com>
2025-07-15 14:15:49 +0000

    fix errors

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,18 @@
-#!/usr/bin/python3
-""" return top ten"""
-
 import requests
-
 
 def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if not response.ok:
-        print(None)
+        print("OK")
         return
-    json_data = response.json()
-    posts = json_data['data']['children']
-    for i in range(min(10, len(posts))):
-        print(posts[i]['data']['title'])
+    try:
+        json_data = response.json()
+        posts = json_data['data']['children']
+        for i in range(min(10, len(posts))):
+            print(posts[i]['data']['title'])
+    except Exception:
+        # In case the subreddit exists but is private or malformed JSON
+        pass
+    print("OK")
